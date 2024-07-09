0ea432582ff00aa83b1e572047b164d0ca88a04a
WillyChristian <crisaort@gmail.com>
2024-07-09 17:25:11 -0300

    feat: add new icons

--- src/theme/components/icons/index.tsx
+++ src/theme/components/icons/index.tsx
@@ -4,17 +4,23 @@ import { SvgProps } from 'react-native-svg';
 import Wallet from '../../../assets/icons/wallet.svg';
 import PlusWhite from '../../../assets/icons/plus-white.svg';
 import MoneySpend from '../../../assets/icons/money-spend.svg';
+import Search from '../../../assets/icons/search.svg';
+import PlusCTA from '../../../assets/icons/plus-cta.svg';
 
 type IconProps = {
   Wallet: React.FC<SvgProps>;
   PlusWhite: React.FC<SvgProps>;
   MoneySpend: React.FC<SvgProps>;
+  Search: React.FC<SvgProps>;
+  PlusCTA: React.FC<SvgProps>;
 };
 
 const Icon: IconProps = {
   Wallet,
   PlusWhite,
   MoneySpend,
+  Search,
+  PlusCTA,
 };
 
 export default Icon;
